03f4adfca3679b4e1d54e275a4e7ac6c4ef06423
Wei Yet <40540799+Weiyet@users.noreply.github.com>
2024-04-21 22:32:27 +0800

    Add files via upload
    Initial Release

---
+++ FIFO/src/fifo.v
@@ -0,0 +1,147 @@
+module fifo #(
+    parameter DEPTH = 12,
+    parameter DATA_WIDTH = 8,
+    parameter ASYNC = 1
+)(
+    input wire rd_clk,
+    input wire wr_clk,
+    input wire rst,
+    input wire [DATA_WIDTH-1:0] data_wr,
+    input wire wr_en,
+    output reg fifo_full,
+    output wire [DATA_WIDTH-1:0] data_rd,
+    input wire rd_en,
+    output wire fifo_empty
+);
+    localparam CNTR_WIDTH = clogb2(DEPTH-1);
+   
+    reg [CNTR_WIDTH-1:0] rd_gray_pointer, rd_binary_pointer;
+    wire [CNTR_WIDTH-1:0] rd_binary_pointer_next;
+    reg [CNTR_WIDTH-1:0] wr_gray_pointer, wr_binary_pointer;
+    wire [CNTR_WIDTH-1:0] wr_binary_pointer_next;
+    reg [DATA_WIDTH-1:0] fifo_stored [DEPTH-1:0];
+    reg [CNTR_WIDTH-1:0] rd_gray_pointer_sync[1:0];
+    reg [CNTR_WIDTH-1:0] wr_gray_pointer_sync[1:0];
+    wire rdptr_eq_next_wrptr;
+    
+    generate //: POINTER_SYNCHRONIZER
+        if (ASYNC == 1) begin
+            always @ (posedge rd_clk, posedge rst) begin
+                if(rst) begin
+                    {wr_gray_pointer_sync[1], wr_gray_pointer_sync[0]} <= {(2*CNTR_WIDTH){1'b0}};
+                end else begin
+                    {wr_gray_pointer_sync[1], wr_gray_pointer_sync[0]} <= {wr_gray_pointer_sync[0], wr_gray_pointer};
+                end
+            end
+
+            always @ (posedge wr_clk, posedge rst) begin
+                if(rst) begin
+                    {rd_gray_pointer_sync[1], rd_gray_pointer_sync[0]} <= {(2*CNTR_WIDTH){1'b0}};
+                end else begin
+                    {rd_gray_pointer_sync[1], rd_gray_pointer_sync[0]} <= {rd_gray_pointer_sync[0], rd_gray_pointer};
+                end
+            end
+        end 
+    endgenerate
+
+    // data_wr, wr_binary_pointer, wr_gray_pointer 
+    assign wr_binary_pointer_next = (wr_binary_pointer == DEPTH-1) ? {CNTR_WIDTH{1'b0}} : wr_binary_pointer + 1;
+    
+    integer i;
+    
+    always @ (posedge wr_clk, posedge rst) begin
+        if(rst) begin
+            wr_binary_pointer <= {CNTR_WIDTH{1'b0}};
+        end else if((wr_en & !rdptr_eq_next_wrptr) | (fifo_full & !rdptr_eq_next_wrptr)) begin
+        // 1. When next write pointer == read pointer AND wr_en, it means last entry of FIFO is being filled, do not update pointer, else update pointer.
+        // 2. Update when fifo_full flag is cleared.
+            wr_binary_pointer <= wr_binary_pointer_next;
+        end
+    end
+    
+    always @ (posedge wr_clk, posedge rst) begin
+        if(rst) begin
+            for (i = 0; i < DEPTH; i = i + 1)
+                fifo_stored[i] <= {DATA_WIDTH{1'b0}};
+        end else if (wr_en & !fifo_full) begin
+            fifo_stored[wr_binary_pointer] <= data_wr;
+        end
+    end
+
+    generate
+        if(ASYNC == 1) begin
+            always @ (posedge wr_clk, posedge rst) begin
+                if(rst) begin
+                    wr_gray_pointer   <= {CNTR_WIDTH{1'b0}};
+                end else if ((wr_en & !rdptr_eq_next_wrptr) | (fifo_full & !rdptr_eq_next_wrptr)) begin
+                // 1. When next write pointer == read pointer AND wr_en, it means last entry of FIFO is being filled, do not update pointer, else update pointer.
+                // 2. Update when fifo_full flag is cleared.
+                    wr_gray_pointer   <= bin_to_gray(wr_binary_pointer_next); 
+                end
+            end
+        end
+    endgenerate
+
+
+    // data_rd, rd_binary_pointer, rd_gray_pointer 
+    assign rd_binary_pointer_next = (rd_binary_pointer == DEPTH-1) ? {CNTR_WIDTH{1'b0}} : rd_binary_pointer + 1;
+    
+    always @ (posedge rd_clk, posedge rst) begin
+        if(rst) begin
+            rd_binary_pointer <= {CNTR_WIDTH{1'b0}};
+        end else if (rd_en & !fifo_empty) begin
+            rd_binary_pointer <= rd_binary_pointer_next;
+        end
+    end
+    
+    assign data_rd = (rd_en & !fifo_empty) ? fifo_stored[rd_binary_pointer] : {DATA_WIDTH{1'b0}}; 
+
+    generate
+        if(ASYNC == 1) begin
+            always @ (posedge rd_clk, posedge rst) begin
+                if(rst) begin
+                    rd_gray_pointer   <= {CNTR_WIDTH{1'b0}};
+                end else if(rd_en & !fifo_empty) begin
+                    rd_gray_pointer   <= bin_to_gray(rd_binary_pointer_next);
+                end
+            end
+        end
+    endgenerate
+
+    // flag
+    generate 
+        if (ASYNC == 1) begin
+            assign rdptr_eq_next_wrptr = (rd_gray_pointer_sync[1] == bin_to_gray(wr_binary_pointer_next));
+            assign fifo_empty = wr_gray_pointer_sync[1] == bin_to_gray(rd_binary_pointer);
+            //assign fifo_full = rd_gray_pointer_sync[1] == bin_to_gray(wr_binary_pointer_next);
+        end else begin
+            assign rdptr_eq_next_wrptr = (rd_binary_pointer == wr_binary_pointer_next);
+            assign fifo_empty = wr_binary_pointer == rd_binary_pointer;
+            //assign fifo_full = rd_binary_pointer == wr_binary_pointer_next;
+        end
+    endgenerate
+    
+    always@(posedge wr_clk, posedge rst) begin
+        if(rst) begin
+            fifo_full <= 1'b0;
+        end else if(wr_en & rdptr_eq_next_wrptr) begin
+        // When next write pointer == read pointer AND wr_en, it means last entry of FIFO is being filled, do not update pointer,
+            fifo_full <= 1'b1;
+        end else if(!rdptr_eq_next_wrptr) begin
+        // Deassert when any read operation is completed: write pointer != read pointer
+            fifo_full <= 1'b0;
+        end
+    end
+    
+    function integer clogb2;
+        input integer value;
+        for(clogb2=0; value>0; value = value>>1)
+             clogb2 = clogb2 + 1;
+    endfunction
+
+    function [CNTR_WIDTH-1:0] bin_to_gray;
+        input [CNTR_WIDTH-1:0] bin;
+        bin_to_gray = bin[CNTR_WIDTH-1:0] ^ (bin[CNTR_WIDTH-1:0] >> 1);   
+    endfunction
+
+endmodule
---
+++ FIFO/tb/sv/tb.v
@@ -0,0 +1,233 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// 
+// Create Date: 04/18/2024 08:27:34 PM
+// Module Name: tb
+// Author: https://www.linkedin.com/in/wei-yet-ng-065485119/
+// Description: 1. fifo_write_burst_rand 
+//              2. fifo_read_burst
+//              3. fifo_burst_rand
+//              4. fifo_read_write_rand_simul
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb(
+);
+parameter DEPTH = 12; // DUT parameter
+parameter DATA_WIDTH = 8; // DUT paramter
+parameter ASYNC = 1; // DUT parameter
+parameter TEST_WEIGHT = 1; // TB multiplier for stimulus injected
+parameter WR_CLK_PERIOD = 20; // TB wr_clk generator
+parameter RD_CLK_PERIOD = 32; // TB rd_clk generator
+parameter SIM_TIMEOUT = 100000; // TB simulation time out
+parameter BURST_LENGHT = DEPTH;
+localparam MAX_DATA = 2**DATA_WIDTH - 1;
+integer fifo_expected [$];
+integer i; 
+integer err_cnt = 0;
+reg rd_clk = 0; 
+reg wr_clk = 0;
+reg rst = 1;
+reg [DATA_WIDTH-1:0] data_wr = 0;
+reg wr_en = 0;
+reg rd_en = 0;
+wire [DATA_WIDTH-1:0] data_rd;
+wire fifo_empty;
+wire fifo_full;
+
+fifo #(
+.DEPTH(DEPTH), 
+.DATA_WIDTH(DATA_WIDTH), 
+.ASYNC(ASYNC)) DUT (
+    /*input wire*/ .rd_clk(rd_clk),
+    /*input wire*/ .wr_clk(wr_clk),
+    /*input wire*/ .rst(rst),
+    /*input wire [DATA_WIDTH-1:0]*/ .data_wr(data_wr),
+    /*input wire*/ .wr_en(wr_en),
+    /*output wire*/ .fifo_full(fifo_full),
+    /*output wire [DATA_WIDTH-1:0]*/ .data_rd(data_rd),
+    /*input wire*/ .rd_en(rd_en),
+    /*output wire*/ .fifo_empty(fifo_empty));
+
+always #(RD_CLK_PERIOD>>1) rd_clk = ~rd_clk;
+always #(WR_CLK_PERIOD>>1) wr_clk = ~wr_clk;
+
+
+integer data_wr_rand = 0;
+
+task fifo_write_burst_rand (input integer count);
+    for (i=0; i<count; i=i+1) begin
+        @(posedge(wr_clk)) begin
+            if(i < DEPTH & fifo_full) begin
+                $display("%0t ERROR: FIFO is not full but fifo_full flag is asserted", $realtime);
+                err_cnt = err_cnt + 1;
+            end
+            wr_en <= 1;
+            data_wr_rand = $urandom_range(0,MAX_DATA);
+            data_wr <= data_wr_rand;
+            if ($size(fifo_expected) < DEPTH) begin 
+                fifo_expected.push_back(data_wr_rand);
+                $display("%0t Data written = %0d, FIFO entry = %0d", $realtime, data_wr_rand, $size(fifo_expected));
+            end else begin // check FIFO FULL flag
+                #1;
+                if(fifo_full) 
+                    $display("%0t FIFO is full, fifo_full flag is asserted correctly", $realtime);
+                else begin
+                    $display("%0t ERROR: FIFO is full but fifo_full flag is not asserted", $realtime);
+                    err_cnt = err_cnt + 1;
+                end
+            end
+        end
+    end
+    @(posedge(wr_clk))
+    wr_en <= 0;
+endtask
+
+integer data_rd_act = 0;
+integer data_rd_exp = 0; 
+
+task fifo_read_burst (input integer count);
+    @(posedge(rd_clk))
+    rd_en <= 1;
+    for (i=0; i<count; i=i+1)  begin
+        @(posedge(rd_clk))begin
+            if ($size(fifo_expected) > 0) begin  
+                if(fifo_empty) begin
+                    $display("%0t ERROR: FIFO is not empty but fifo_empty flag is asserted", $realtime);
+                    err_cnt = err_cnt + 1;
+                end
+                data_rd_exp = fifo_expected.pop_front();
+                data_rd_act <= data_rd; 
+                #1; //to make sure data_rd_act capture data_rd signal.
+                if(data_rd_exp == data_rd_act) 
+                    $display("%0t Data read = %d, FIFO entry = %d", data_rd_act, $realtime, $size(fifo_expected));
+                else begin
+                    $display("%0t EERROR: Data read mismatch, ACT = %d, EXP =%d, FIFO entry = %d", $realtime, data_rd_act, data_rd_exp, $size(fifo_expected));
+                    err_cnt = err_cnt + 1;
+                end        
+            end else begin // check FIFO EMPTY flag
+                #1;
+                if(fifo_empty) 
+                    $display("%0t FIFO is empty, fifo_empty flag is asserted correctly", $realtime);
+                else begin
+                    $display("%0t ERROR: FIFO is empty but fifo_empty flag is not asserted", $realtime);
+                    err_cnt = err_cnt + 1;
+                end
+            end
+        end
+    end
+//    @(posedge(rd_clk))
+    rd_en <= 0;
+endtask
+
+integer op_count = 0;
+integer j = 0;
+bit op_sel = 0;
+
+task fifo_burst_rand(int count);
+    j = count;
+    while(j >= 0) begin
+      op_sel = $random(); 
+      op_count = $urandom_range(1,j); // to have continuous request
+      j = j - op_count;
+      case(op_sel) 
+        1: begin // read
+            fifo_read_burst(op_count);
+            #RD_CLK_PERIOD;
+            #(3*WR_CLK_PERIOD); // It might take extra cycle for rd_pointer synchronization to deassert fifo_full, used for write operation
+        end
+        0: begin // write 
+            fifo_write_burst_rand(op_count);
+            #WR_CLK_PERIOD;
+            #(3*RD_CLK_PERIOD); // It might take extra cycle for wr_pointer synchronization to deassert fifo_empty, used for read operation
+        end
+      endcase 
+    end
+endtask
+
+integer fifo_wr_stream [$];
+integer fifo_rd_stream [$];
+
+
+task fifo_read_write_rand_simul();
+    fifo_read_burst($size(fifo_expected)); // to make sure FIFO is empty   
+    fifo_wr_stream = {};
+    fifo_rd_stream = {};
+    for(i = 0; i < DEPTH; i = i+1) begin
+        data_wr_rand = $urandom_range(0, MAX_DATA+1);
+        fifo_wr_stream.push_back(data_wr_rand);
+    end
+    fork 
+        begin
+           for (i=0; i<BURST_LENGHT; i=i+1) begin  
+                @(posedge(wr_clk))
+                wr_en <= 1;
+                data_wr <= fifo_wr_stream[i];
+                fifo_expected.push_back(fifo_wr_stream[i]);
+                $display("%0t Data written = %d, FIFO entry = %d", $realtime, fifo_wr_stream[i], $size(fifo_expected));
+           end
+           @(posedge(wr_clk))
+           wr_en = 0;
+        end
+        begin
+           @(posedge(rd_clk))
+           rd_en = 1;
+           while($size(fifo_wr_stream) != DEPTH)begin
+              @(posedge(rd_clk))
+              if(!fifo_empty) begin
+                 data_rd_act <= data_rd;
+                 fifo_rd_stream.push_back(data_rd_act);
+                 fifo_expected.pop_front();
+                 $display("%0t Data read = %d, FIFO entry = %d", $realtime, data_rd_act, $size(fifo_expected));
+              end 
+           end
+//           @(posedge(rd_clk))
+           rd_en = 0;        
+        end
+    join
+    for(i=0;i<DEPTH;i=i+1) begin
+        if (fifo_wr_stream[i] != fifo_rd_stream[i]) begin
+            $display("%0t ERROR data_rd %d does not match data_wr %d", $realtime, fifo_rd_stream[i], fifo_wr_stream[i]);
+            err_cnt = err_cnt + 1;
+        end
+    end
+endtask
+
+initial begin
+    rst = 1;
+    #100 
+    rst = 0;
+    repeat(TEST_WEIGHT) begin
+        $display("\n%0t FIFO WRITE BURST SEQ",$realtime);
+        fifo_write_burst_rand(DEPTH+3);
+        #1000;
+        $display("\n%0t FIFO READ BURST SEQ",$realtime);
+        fifo_read_burst(DEPTH+3);
+        #1000;
+    end
+    $display("\n%0t FIFO RANDOM READ WRITE SEQ",$realtime);
+    fifo_burst_rand(TEST_WEIGHT*DEPTH);
+    #1000;
+    repeat(TEST_WEIGHT) begin
+        $display("\n%0t FIFO SIMULTANEOUS RANDOM READ WRITE SEQ",$realtime);
+        fifo_read_write_rand_simul();
+    end
+    #1000;
+    if (err_cnt > 0) begin
+        $display("\n%0t TEST FAILED",$realtime);
+        $display("Error count = %d\n", err_cnt);
+    end else
+        $display("\n%0t TEST PASSED\n", $realtime);
+    $stop;
+end
+
+initial begin
+    #(SIM_TIMEOUT);
+    $display("\n%0t TEST FAILED", $realtime);
+    $display("SIM TIMEOUT!\n");
+    $stop;
+end
+
+endmodule
