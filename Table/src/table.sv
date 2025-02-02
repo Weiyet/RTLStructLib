86fcc6d7d156c49d48521186054b7f4bd1559b31
weiyng <weiyng@DESKTOP-0SK27FN>
2025-02-02 16:32:39 +0800

    Fix Makefile rules, Fix reset error of DUT

--- Table/src/table.sv
+++ Table/src/table.sv
@@ -26,26 +26,27 @@ module table_top #(
 );
     localparam INDEX_WIDTH = $clog2(TABLE_SIZE-1);
     reg [DATA_WIDTH-1:0] data_stored [TABLE_SIZE];
+    integer i;
 
-    always @ (posedge clk or negedge rst) begin
+    always @ (posedge clk or posedge rst) begin
         if(rst) begin
-            for (int i=0; i<TABLE_SIZE; i=i+1) begin
+            for (i=0; i<TABLE_SIZE; i=i+1) begin
                 data_stored[i] = 'b0;
             end
         end else if (wr_en) begin
-            for (int i=1; i<=INPUT_RATE; i=i+1) begin
+            for (i=1; i<=INPUT_RATE; i=i+1) begin
                 data_stored[index_wr[i*INDEX_WIDTH-1 -:INDEX_WIDTH]] = data_wr[i*DATA_WIDTH-1 -:DATA_WIDTH];
             end            
         end
     end
 
-    always @ (posedge clk or negedge rst) begin 
+    always @ (posedge clk or posedge rst) begin 
         if(rst) begin
-            for (int i=1; i<=OUTPUT_RATE; i=i+1) begin
+            for (i=1; i<=OUTPUT_RATE; i=i+1) begin
                 data_rd[i*DATA_WIDTH-1 -: DATA_WIDTH] = 'b0;
             end
         end else if (rd_en) begin
-            for (int i=1; i<=OUTPUT_RATE; i=i+1) begin
+            for (i=1; i<=OUTPUT_RATE; i=i+1) begin
                 data_rd[i*DATA_WIDTH-1 -: DATA_WIDTH] = data_stored[index_rd[i*INDEX_WIDTH-1 -:INDEX_WIDTH]];
             end            
         end
