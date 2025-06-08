c6548304379bb96b1fff4e5c9c7c0bd2d4b865f6
weiyng <weiyng@DESKTOP-0SK27FN>
2025-06-08 21:12:09 +0800

    Intial Tb, fix basic insert, read op

--- List/src/list.sv
+++ List/src/list.sv
@@ -3,7 +3,7 @@
 // Module Name: list
 // Create Date: 07/05/2025 07:51 AM
 // Author: https://www.linkedin.com/in/wei-yet-ng-065485119/
-// Last Update: 14/05/2025 09:05 PM
+// Last Update: 08/06/2025 04:21 PM
 // Last Updated By: https://www.linkedin.com/in/wei-yet-ng-065485119/
 // Description: List 
 // Additional Comments: 
@@ -15,11 +15,11 @@ module list #(
     parameter DATA_WIDTH = 32,
     parameter LENGTH = 8,
     parameter SUM_METHOD = 0, // 0: parallel (combo) sum, 1: sequentia sum, 2: adder tree. //ICARUS does not support string overriden to parameter in CLI. 
-    localparam LENGTH_WIDTH =  $clog2(LENGTH+1)
+    localparam LENGTH_WIDTH =  $clog2(LENGTH)
 )(
     input  wire                                clk,
     input  wire                                rst,
-    input  wire [1:0]                          op_sel,
+    input  wire [2:0]                          op_sel,
     input  wire                                op_en,
     input  wire [DATA_WIDTH-1:0]               data_in,
     input  wire [LENGTH_WIDTH-1:0]             index_in,
@@ -30,13 +30,13 @@ module list #(
 );
 
   //  localparam LENGTH_WIDTH = $clog2(LENGTH+1) 
-    
     localparam IDLE        = 3'b000;
     localparam SUM         = 3'b001;
     localparam SORT        = 3'b010;
-    localparam SEARCH      = 3'b011;
+    localparam SEARCH_1ST  = 3'b011;
     localparam SEARCH_ALL  = 3'b101;
     localparam ACCESS_DONE = 3'b100;
+    localparam ACCESS_DONE2= 3'b110; 
     
     reg [$clog2(LENGTH+1)-1:0]       data_count;
     reg [LENGTH-1:0][DATA_WIDTH-1:0] data_stored; // could implement with RAM for large size of data
@@ -44,8 +44,10 @@ module list #(
     reg [LENGTH_WIDTH-1:0]           cur_ptr;
     reg [2:0]                        current_state;
     reg                              found;
-    wire                             op_is_write; 
+    reg                              dly_cnt; 
+    wire                             op_is_insert; 
     wire                             op_is_read;
+    wire                             op_is_delete;
     wire                             op_is_find_all_index;
     wire                             op_is_find_1st_index;
     wire                             op_is_sum;
@@ -66,12 +68,13 @@ module list #(
     integer i;
     
     assign op_is_read = (op_sel == 3'b000) & op_en;
-    assign op_is_write = (op_sel == 3'b001) & op_en;
+    assign op_is_insert = (op_sel == 3'b001) & op_en;
     assign op_is_find_all_index = (op_sel == 3'b010) & op_en;
     assign op_is_find_1st_index = (op_sel == 3'b011) & op_en;
     assign op_is_sum = (op_sel == 3'b100) & op_en;
     assign op_is_sort_asc = (op_sel == 3'b101) & op_en;
     assign op_is_sort_des = (op_sel == 3'b110) & op_en;
+    assign op_is_delete = (op_sel == 3'b111) & op_en; //not implemented yet
     
 //    always @ (*) begin
 //        data_stored_packed = {<< DATA_WIDTH {data_stored}};
@@ -89,7 +92,7 @@ module list #(
               .sum_in_progress(sum_in_progress));
               
     sorter #(.DATA_WIDTH(DATA_WIDTH),
-           .LENGTH(LENGTH))
+             .LENGTH(LENGTH))
     u_sorter (.clk(clk),
             .rst(rst),
             .data_in(data_stored),
@@ -98,18 +101,6 @@ module list #(
             .sort_done(sort_done),
             .sort_in_progress(sort_in_progress),
             .data_sorted(data_sorted));
-     
-    always @ (posedge clk, posedge rst) begin
-        if(rst) begin
-            for(i = 0; i < LENGTH; i++) begin
-                data_stored[i] <= {(DATA_WIDTH){1'b0}};    
-            end     
-            data_count = {(LENGTH_WIDTH){1'b0}};
-        end else if (op_is_write) begin
-            data_stored[index_in] <= data_in;
-            data_count <= data_count + 1 ;
-        end
-    end
     
     always @ (posedge clk, posedge rst) begin
        if(rst) begin
@@ -121,18 +112,50 @@ module list #(
           data_out <= 'b0;
           cur_ptr <= 'b0;
           found <= 1'b0;
+          data_count = {(LENGTH_WIDTH){1'b0}};
+          for(i = 0; i < LENGTH; i++) begin
+            data_stored[i] <= {(DATA_WIDTH){1'b0}};    
+          end 
        end else begin
           case(current_state) 
             IDLE: begin
-                if(op_is_write) begin
-                    current_state <= ACCESS_DONE;
-                    op_done <= 1'b1;
-                    op_error <= 1'b0;
+                if(op_is_insert) begin
+                    if(data_count >= LENGTH) begin
+                        current_state <= ACCESS_DONE; //LENGTH is full, cannot insert
+                        op_done <= 1'b1;
+                        op_error <= 1'b1;
+                    end else if (index_in >= data_count) begin
+                        current_state <= ACCESS_DONE; 
+                        data_stored[data_count] <= data_in; //insert data at the end
+                        data_count <= data_count + 1; //increment data count
+                        op_done <= 1'b1;
+                        op_error <= 1'b0;
+                    end else begin
+                        current_state <= ACCESS_DONE; //insert data at index_in
+                        for(i = 0; i >= LENGTH; i = i + 1) begin
+                            if(i == index_in) begin
+                                data_stored[LENGTH-1-i] <= data_in; //insert data at index_in
+                            end else if(i > index_in) begin
+                                data_stored[LENGTH-1-i] <= data_stored[LENGTH-1-i-1]; //shift right
+                            end else begin
+                                data_stored[LENGTH-1-i] <= data_stored[LENGTH-1-i]; //keep the same
+                            end
+                        end
+                        data_count <= data_count + 1; //increment data count
+                        op_done <= 1'b1;
+                        op_error <= 1'b0;
+                    end
                 end else if (op_is_read) begin
-                    current_state <= ACCESS_DONE;
-                    data_out <= {{LENGTH_WIDTH{1'b0}},data_stored[index_in]}; //need buffer stage if RAM instances is used for data_stored.
-                    op_done <= 1'b1;
-                    op_error <= 1'b0;
+                    if (index_in >= data_count) begin
+                        current_state <= ACCESS_DONE; //index_in is out of range
+                        op_done <= 1'b1;
+                        op_error <= 1'b1;
+                    end else begin
+                        current_state <= ACCESS_DONE;
+                        data_out <= {{LENGTH_WIDTH{1'b0}},data_stored[index_in]};
+                        op_done <= 1'b1;
+                        op_error <= 1'b0;
+                    end
                 end else if (op_is_sum) begin
                     if(SUM_METHOD == 0) begin //PARALLEL SUM
                         current_state <= ACCESS_DONE;
@@ -149,7 +172,7 @@ module list #(
                         sum_en <= 1'b1;
                     end
                 end else if (op_is_find_1st_index) begin               
-                    current_state <= SEARCH;
+                    current_state <= SEARCH_1ST;
                     op_in_progress <= 1'b1;
                     cur_ptr <= 'd0;
                 end else if (op_is_find_all_index) begin               
@@ -178,10 +201,11 @@ module list #(
                    op_in_progress <= 1'b0;
                    sum_en <= 1'b0;
                    sort_en <= 1'b0;
-                   found <= 1'b0;
                 end
+                found <= 1'b0;
+                cur_ptr <= 'b0;
             end
-          SEARCH: begin
+          SEARCH_1ST: begin
                     if(data_stored[cur_ptr] == data_in) begin
                         current_state <= ACCESS_DONE;
                         data_out <= {{DATA_WIDTH{1'b0}},cur_ptr};
@@ -222,7 +246,8 @@ module list #(
           end
           SORT: if(sort_done) begin
                    current_state <= ACCESS_DONE;
-                   op_done <= 1'b0;
+                   op_done <= 1'b1;
+                   data_stored <= data_sorted;
                    op_in_progress <= 1'b0;
                    op_error <= 1'b0;
                    sort_en <= 1'b0;
@@ -239,6 +264,7 @@ module list #(
                        current_state <= IDLE;
                        op_done <= 1'b0;
                        op_error <= 1'b0;
+                       op_in_progress <= 1'b0;
           end
           default: begin
                    current_state <= IDLE;
---
+++ List/tb/sv/tb.sv
@@ -0,0 +1,373 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Create Date: 08/06/2025 09:10 PM
+// Module Name: tb
+// Description: Supported Operation 
+//             0. Read(index_in) -> data_out 
+//             1. Insert(index_in, data_in) 
+//             2. Find_all_index(index_in) -> data_out (array of indexes)
+//             3. Find_1st_index(index_in) -> data_out (index of first occurrence)
+//             4. Sum() -> data_out (sum of all elements)
+//             5. Sort_Asc()
+//             6. Sort_Des() 
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb(
+    );
+    // DUT parameter
+    localparam DUT_DATA_WIDTH = 8;
+    localparam DUT_LENGTH = 8; // Max number of nodes in the list
+    localparam DUT_SUM_METHOD = 0; // 0: parallel (combo) sum, 1: sequentia sum, 2: adder tree. //ICARUS does not support string overriden to parameter in CLI.
+
+    // TB parameter
+    localparam TB_CLK_PERIOD = 25;
+    localparam TB_TEST_WEIGHT = 1;
+    localparam SIM_TIMEOUT = 500000;
+
+localparam LENGTH_WIDTH =  $clog2(DUT_LENGTH);
+localparam DATA_OUT_WIDTH = LENGTH_WIDTH+DUT_DATA_WIDTH-1;
+
+localparam OP_Read = 3'b000; 
+localparam OP_Insert = 3'b001;
+localparam OP_Find_all_index = 3'b010;
+localparam OP_Find_1st_index = 3'b011;
+localparam OP_Sum = 3'b100;
+localparam OP_Sort_Asc = 3'b101;
+localparam OP_Sort_Des = 3'b110;
+localparam OP_Delete = 3'b111;
+
+integer err_cnt = 0;
+
+// input 
+reg clk = 0;
+reg rst = 0;
+reg [2:0] op_sel = 0; 
+reg op_en = 0; // Enable operation
+reg [DUT_DATA_WIDTH-1:0] data_in = 0;
+reg [LENGTH_WIDTH-1:0] index_in = 0; // Index for Insert_At_Index and Delete_At_Index
+// output
+wire [DUT_DATA_WIDTH+LENGTH_WIDTH-1:0] data_out; // Data out for Read and Insert_At_Index
+wire op_done; // Operation done flag
+wire op_in_progress; // Operation in progress flag
+wire op_error; // Operation error flag
+
+
+`ifdef XILINX_GLS
+    // for xilinx gate sim
+    glbl glbl();
+`endif
+
+   list #(
+      .DATA_WIDTH(DUT_DATA_WIDTH),
+      .LENGTH(DUT_LENGTH),
+      .SUM_METHOD(DUT_SUM_METHOD)
+   ) DUT (
+      /*input  wire                              */.clk(clk),
+      /*input  wire                              */.rst(rst),
+      /*input  wire [1:0]                        */.op_sel(op_sel),
+      /*input  wire                              */.op_en(op_en),
+      /*input  wire [DATA_WIDTH-1:0]             */.data_in(data_in),
+      /*input  wire [LENGTH_WIDTH-1:0]           */.index_in(index_in),	
+      /*output reg  [LENGTH_WIDTH+DATA_WIDTH-1:0]*/.data_out(data_out),  
+      /*output reg                               */.op_done(op_done),
+      /*output reg                               */.op_in_progress(op_in_progress),
+      /*output reg                               */.op_error(op_error)
+   );
+    
+always #(TB_CLK_PERIOD/2) clk = ~clk; 
+
+integer list_exp[$]; // expected list contents
+integer temp[$]; // temporary storage for find_index results
+
+task list_print_contents();
+   for (integer i = 0; i < list_exp.size(); i = i + 1) begin
+      $write("%0d ", list_exp[i]); // $write does not add newline
+   end
+   $write("\n");
+endtask 
+
+task find_index (input integer value); // input integer list[$], ref integer addr[$]); icarus does not support "ref" (pass by reference).
+begin
+   temp = {};
+   for (integer i = 0; i < list_exp.size(); i = i + 1) begin
+      if(value == list_exp[i]) begin
+         temp.push_back(i);
+         $display("%0t value %0d is found at Index %0d", $realtime, value, i);
+      end
+   end
+end
+endtask
+
+task read (input integer index);
+begin
+   $display("%0t OP_Read at index %0d", $realtime,index); 
+   @(posedge (clk));
+   #1 
+   op_sel = OP_Read;  
+   op_en = 1;
+   index_in = index;
+   @(posedge (clk));
+   #1
+   wait (op_done); 
+   if(index >= list_exp.size()) begin
+      if(op_error) begin
+         $display("%0t Data read out of bound, fault flag is asserted correctly",$realtime);
+      end else begin
+         $error("%0t Data read out of bound, fault flag is not asserted",$realtime);
+         err_cnt = err_cnt + 1;
+      end
+   end else begin
+      if(op_error) begin
+         $error("%0t fault flag is asserted incorrectly",$realtime);
+         err_cnt = err_cnt + 1;
+      end
+      if(data_out == list_exp[index]) begin
+         $display("%0t Data read: %0d",$realtime,data_out);
+      end else begin
+         $error("%0t Data read: %0d, Data Exp: %0d", $realtime, data_out, list_exp[index]);
+         err_cnt = err_cnt + 1; 
+      end
+   end
+   @(posedge (clk));
+   #1
+   op_en = 0;
+end
+endtask
+
+integer count = 0;
+
+task read_n_burst(input integer n);
+begin
+   $display("%0t OP_Read burst %d time", $realtime,n); 
+   count = 0;
+   @(posedge (clk));
+   #1 
+   op_sel = OP_Read;  
+   op_en = 1;
+   index_in = count;
+   while (count < n) begin
+      @(posedge (clk));
+      #1
+      wait (op_done); 
+      if(index_in >= list_exp.size()) begin
+         if(op_error) begin
+            $display("%0t Data read out of bound, fault flag is asserted correctly",$realtime);
+         end else begin
+            $error("%0t Data read out of bound, fault flag is not asserted",$realtime);
+            err_cnt = err_cnt + 1;
+         end
+      end else begin
+         if(op_error) begin
+            $error("%0t fault flag is asserted incorrectly",$realtime);
+            err_cnt = err_cnt + 1;
+         end
+         if(data_out == list_exp[index_in]) begin
+            $display("%0t Data read: %0d",$realtime,data_out);
+         end else begin
+            $error("%0t Data read: %0d, Data Exp: %0d", $realtime, data_out, list_exp[index_in]);
+            err_cnt = err_cnt + 1; 
+         end
+      end
+      count = count + 1;
+      index_in = count;
+   end
+   @(posedge (clk));
+   #1
+   op_en = 0;
+end
+endtask
+
+task insert(input integer index, input integer value);
+begin
+   $display("%0t OP_Insert at index %d, value %d", $realtime,index,value); 
+   @(posedge (clk));
+   #1 
+   op_sel = OP_Insert;  
+   op_en = 1;
+   index_in = index;
+   data_in = value;
+   @(posedge (clk));
+   #1
+   wait (op_done); 
+   if(list_exp.size() >= DUT_LENGTH) begin
+      if(op_error) begin
+         $display("%0t Data insert out of bound, fault flag is asserted correctly",$realtime);
+      end else begin
+         $error("%0t Data insert out of bound, fault flag is not asserted",$realtime);
+         err_cnt = err_cnt + 1;
+      end
+   end else begin
+      if(op_error) begin
+         $error("%0t fault flag is asserted incorrectly",$realtime);
+         err_cnt = err_cnt + 1;
+      end
+      if(index > list_exp.size()) begin
+         list_exp.push_back(value); // Insert at the end if index is out of bound
+      end else begin
+         list_exp.insert(index, value);
+      end
+   end
+   @(posedge (clk));
+   #1
+   op_en = 0;
+   list_print_contents();
+end
+endtask
+
+task delete(input integer index);
+begin
+    $display("%0t OP_Delete at index %d", $realtime,index); 
+    @(posedge (clk));
+    #1 
+    op_sel = OP_Delete;  
+    op_en = 1;
+    index_in = index;
+    @(posedge (clk));
+    #1
+    wait (op_done); 
+    if(index >= list_exp.size()) begin
+       if(op_error) begin
+          $display("%0t Data delete out of bound, fault flag is asserted correctly",$realtime);
+       end else begin
+          $error("%0t Data delete out of bound, fault flag is not asserted",$realtime);
+          err_cnt = err_cnt + 1;
+       end
+    end else begin
+       if(op_error) begin
+          $error("%0t fault flag is asserted incorrectly",$realtime);
+          err_cnt = err_cnt + 1;
+       end
+       list_exp.delete(index);
+    end
+    @(posedge (clk));
+    #1
+    op_en = 0;
+    list_print_contents();
+end
+endtask
+
+task sort_acending();
+begin
+   int temp;
+   $display("%0t OP_Sort_Asc Request", $realtime); 
+   @(posedge (clk));
+   #1 
+   op_sel = OP_Sort_Asc;  
+   op_en = 1;
+   @(posedge (clk));
+   #1
+   wait (op_done); 
+   for (integer i = 0; i < list_exp.size() - 1; i = i + 1) begin
+      for (integer j = i + 1; j < list_exp.size(); j = j + 1) begin
+         if(list_exp[i] > list_exp[j]) begin
+            temp = list_exp[i];
+            list_exp[i] = list_exp[j];
+            list_exp[j] = temp;
+         end
+      end
+   $display("%0t OP_Sort_Asc Complete", $realtime); 
+   end
+   @(posedge (clk));
+   #1
+   op_en = 0;
+   list_print_contents();
+end
+endtask
+
+task sort_desending();
+begin
+   int temp;
+   $display("%0t OP_Sort_Des Request", $realtime); 
+   @(posedge (clk));
+   #1 
+   op_sel = OP_Sort_Des;  
+   op_en = 1;
+   @(posedge (clk));
+   #1
+   wait (op_done); 
+   for (integer i = 0; i < list_exp.size() - 1; i = i + 1) begin
+      for (integer j = i + 1; j < list_exp.size(); j = j + 1) begin
+         if(list_exp[i] < list_exp[j]) begin
+            temp = list_exp[i];
+            list_exp[i] = list_exp[j];
+            list_exp[j] = temp;
+         end
+      end
+   end
+   $display("%0t OP_Sort_Des Complete", $realtime); 
+   @(posedge (clk));
+   #1
+   op_en = 0;
+   list_print_contents();
+end
+endtask
+
+task direct_op_test();
+begin
+   $display("Starting direct operation test");
+
+   // Insert operation
+   insert($urandom_range(0,2**DUT_DATA_WIDTH-1), 0);
+   insert($urandom_range(0,2**DUT_DATA_WIDTH-1), 1);
+   insert($urandom_range(0,2**DUT_DATA_WIDTH-1), 2); // Out of bound insert
+   
+   // Read operation
+   read(0);
+   read(1);
+   read(2);
+
+   // // Sort Ascending
+   // sort_acending();
+
+   // // Read after sort
+   // read_n_burst(3);
+
+   // // Sort Descending
+   // sort_desending();
+
+   // // Read after sort
+   // read_n_burst(3);
+
+end
+endtask
+
+initial begin
+    string vcdfile;
+    int vcdlevel;
+    int seed;
+    int temp;
+
+    rst = 1'b1;
+    if ($value$plusargs("VCDFILE=%s",vcdfile))
+        $dumpfile(vcdfile);
+    if ($value$plusargs("VCDLEVEL=%d",vcdlevel))
+        $dumpvars(vcdlevel,tb);
+    if ($value$plusargs("SEED=%d",seed)) begin
+        temp = $urandom(seed);
+        $display("Seed = %d",seed);
+    end
+    #100;
+    rst = 1'b0;
+    direct_op_test();
+    
+    #1000;
+    
+    if (err_cnt > 0) begin
+        $display("\n%0t TEST FAILED",$realtime);
+        $display("Error count = %d\n", err_cnt);
+    end else
+        $display("\n%0t TEST PASSED\n", $realtime);
+    $finish;
+end
+
+initial begin
+    #(SIM_TIMEOUT)
+    $display("\n%0t TEST FAILED", $realtime);
+    $display("SIM TIMEOUT!\n");
+    $finish;
+end
+
+endmodule
