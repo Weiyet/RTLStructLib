8d383daf66e37dccfbe11420579fe0c1fa498817
Weiyet <ngweiyet@gmail.com>
2025-08-10 17:12:19 +0800

    initial check in

--- Systolic_Array/src/systolic_array_top.sv
+++ Systolic_Array/src/systolic_array_top.sv
@@ -1,1 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+//
+// Create Date: 07/29/2025 08:48:20 PM
+// Design Name: 
+// Module Name: systolic_array_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+
+module systolic_array_top #(
+    parameter LEFT_MATRIX_ROW = 50,
+    parameter INNER_DIMENSION = 50,
+    parameter RIGHT_MATRIX_COL = 50,
+    parameter DATA_WIDTH = 8 
+    )(
+    );
+endmodule
